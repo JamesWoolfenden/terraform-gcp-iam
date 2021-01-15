28e9d9d4f2a403bbcfdd0618a6586bbf916b715b
James Woolfenden <James.Woolfenden@gmail.com>
2021-01-15 22:30:40 +0000

    refresh

--- example/examplea/provider.google.tf
+++ example/examplea/provider.google.tf
@@ -1,5 +1,4 @@
 provider "google" {
   project = var.project
   region  = var.region
-  version = "3.43.0"
 }
---
+++ example/examplea/terraform.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    google = {
+      version = "3.52.0"
+      source  = "hashicorp/google"
+    }
+  }
+}
